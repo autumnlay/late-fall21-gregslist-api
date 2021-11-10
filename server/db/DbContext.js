f91b74b772dab4ba5af2cc4ef993057780ac5574
Autumn Lay <autumn.lay0@gmail.com>
2021-11-10 14:28:48 -0700

    random update

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -8,7 +8,7 @@ class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
 
-  //had to add this part in!!
+  // had to add this part in!!
   Houses = mongoose.model('House', HouseSchema)
   Cars = mongoose.model('Car', CarSchema)
   // Profiles is the same collection as account and therefore acts as a 'reducer'
--- server/services/HousesService.js
+++ server/services/HousesService.js
@@ -1,27 +1,32 @@
-import { dbContext } from "../db/DbContext";
-import { BadRequest } from "../utils/Errors";
+import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
 
 class HousesService {
-    async getAll(query = {}) {
-        //REVIEW what is passed through the .populate()? what goes on the inside there??
-        const houses = await dbContext.Houses.find(query).populate('creator', 'name picture')
-        return houses
-    }
+  async getAll(query = {}) {
+    // REVIEW what is passed through the .populate()? what goes on the inside there, as the parameters??
+    // NOTE the dbCOntext refrences that in our server. db. dbContext.
+    // Houses refrenses our Schema??
+    // .find is a js method.
+    // pass in query.
+    // .populate does ____
+    const houses = await dbContext.Houses.find(query).populate('creator', 'name picture')
+    return houses
+  }
 
-    //REVIEW how is this a 'get' function with out it being a .get()??
-    async getById(id) {
-        const house = await dbContext.Houses.findById(id)
-        //if NOT! car
-        if (!house) {
-            throw new BadRequest('Invalid House Id')
-        } return house
-    }
+  // REVIEW how is this a 'get' function with out it being a .get()??
+  async getById(id) {
+    const house = await dbContext.Houses.findById(id)
+    // if NOT! car
+    if (!house) {
+      throw new BadRequest('Invalid House Id')
+    } return house
+  }
 
-    //REVIEW this body that is passed through, what is it?? where did it come from?? Is it connected to anything?
-    async create(body) {
-        const house = await dbContext.Houses.create(body)
-        return house
-    }
+  // REVIEW this body that is passed through, what is it?? where did it come from?? Is it connected to anything?
+  async create(body) {
+    const house = await dbContext.Houses.create(body)
+    return house
+  }
 }
 
 export const housesService = new HousesService()
